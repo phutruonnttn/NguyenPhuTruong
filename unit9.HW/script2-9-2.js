724c436f37a61c27c0d14aff3c057758639587f9
Truong Nguyen <phutruongnttn@gmail.com>
2019-01-22 20:36:57 +0700

    Nop bai tap buoi 9

--- unit9.HW/script2-9-2.js
+++ unit9.HW/script2-9-2.js
@@ -2,21 +2,12 @@ var input = document.getElementsByClassName("ip");
 for (var i = 0; i < input.length; i++) {
 	document.getElementsByClassName("ip")[i].addEventListener("click", function(){
 		var t = this.firstElementChild;
-		if (t.checked == true) {
-			t.checked = false;
-		} else {
-			t.checked = true;
-		}
+		t.checked = 1 - t.checked;
 	})}
 var ck = document.getElementsByClassName("check");
 for (var i = 0; i < ck.length; i++) {
 	document.getElementsByClassName("check")[i].addEventListener("click", function(){
-		var t = this;
-		if (t.checked == true) {
-			t.checked = false;
-		} else {
-			t.checked = true;
-		}
+		this.checked = 1 - this.checked;
 	})}
 function check(){
 	var check = document.getElementsByClassName("check");
